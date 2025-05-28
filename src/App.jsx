603793158c56b273ba5e7560b1275a9b20b4be1e
PihuSaini152 <sainipihu152@gmail.com>
2025-05-28 08:12:31 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navebar/nav.jsx';
 import Footer from './Components/Footer/Footer.jsx'; 
-import Home from './Pageg/Herosection/herosection.jsx'; 
-import Services from './Pageg/services/service.jsx';
-import About from './Pageg/About/about.jsx'
-import Gallery from './Pageg/Eventgallery/eventgallery.jsx';
-import Contact from './Pageg/contect/contect.jsx'
+import Home from './Page/Herosection/herosection.jsx'; 
+import Services from './Page/services/service.jsx';
+import About from './Page/About/about.jsx'
+import Gallery from './Page/Eventgallery/eventgallery.jsx';
+import Contact from './Page/Contact/contect.jsx'
 
 function App() {
   return (
